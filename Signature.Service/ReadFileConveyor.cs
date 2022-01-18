90a0224744027ab83928ccc21017532462b6c5be
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2022-01-18 23:59:18 +0300

    Added thread state flags

--- Signature.Service/ReadFileConveyor.cs
+++ Signature.Service/ReadFileConveyor.cs
@@ -3,41 +3,64 @@ using Signature.Service.Models;
 
 namespace Signature.Service;
 
-public class ReadFileConveyor
+public class ReadFileConveyor : IDisposable
 {
-    public readonly ConcurrentQueue<Chunk> ChunkQueue = new ConcurrentQueue<Chunk>();
+    public ConcurrentQueue<Chunk> ChunkQueue { get; } = new ConcurrentQueue<Chunk>();
+
+    public AutoResetEvent AnyChunkEvent { get; } = new AutoResetEvent(false);
+
+    public AutoResetEvent BufferFreeEvent { get; } = new AutoResetEvent(true);
+
+    private int maxBufferSize { get; }
 
     private bool IsReadComplited { get; set; }
 
     public bool IsComplited => (IsReadComplited && ChunkQueue.IsEmpty);
 
-    public bool HasAnyChunk => !ChunkQueue.IsEmpty;
-
-    public int StackCount => ChunkQueue.Count();
-
-    public ReadFileConveyor()
+    public ReadFileConveyor(int bussefSize)
     {
         ChunkQueue.Clear();
+        maxBufferSize = bussefSize;
         IsReadComplited = false;
     }
 
     public Chunk? DequeueChunk()
     {
-        if (IsComplited)
+        if (ChunkQueue.IsEmpty && !IsReadComplited)
         {
+            AnyChunkEvent.Reset();
             return null;
         }
 
         ChunkQueue.TryDequeue(out Chunk? chunk);
 
         /* TODO Create Custom Exception */
-        return chunk is null ? throw new Exception() : chunk;
+        //return chunk is null ? throw new Exception() : chunk;
+        BufferFreeEvent.Set();
+        return chunk;
     }
 
-    public void PushChunk(Chunk chunk)
+    public void EnqueueChunk(Chunk chunk)
     {
         ChunkQueue.Enqueue(chunk);
+
+        if (ChunkQueue.Count >= maxBufferSize)
+        {
+            BufferFreeEvent.Reset();
+        }
+
+        AnyChunkEvent.Set();
     }
 
-    public void ReadComplited() => IsReadComplited = true;
+    public void ReadComplited()
+    {
+        IsReadComplited = true;
+        AnyChunkEvent.Set();
+    }
+
+    public void Dispose()
+    {
+        AnyChunkEvent.Dispose();
+        BufferFreeEvent.Dispose();
+    }
 }
